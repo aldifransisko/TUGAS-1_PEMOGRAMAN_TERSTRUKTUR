7701e20789832dd4e1e9f6135571244e3a67edca
caseus <79889875+aldifransisko@users.noreply.github.com>
2021-03-03 22:32:40 +0700

    tugas_1dec++_kelompok_8
    tugas_1dec++_kelompok_8

--- tugas1devc++.cpp
+++ tugas1devc++.cpp
@@ -24,33 +24,44 @@ int main(){
 	cin>>pilih;
     cout<<"==========================="<<endl;
 
-       switch(pilih){
-        case '1' :
-                   cout<<"Menghitung luas persegi"<<endl;
-                   cout<<"Sisi = ";
-				   cin>>sisi;
-                   cout<<"Luas persegi = "<<sisi*sisi;
-              break;
-        case '2' :
-                   cout<<"Menghitung luas segitiga"<<endl;
-                   cout<<"Alas = ";
-				   cin>>alas;
-                   cout<<"Tinggi = ";
-				   cin>>tinggi;
-                   cout<<"Luas segitiga = "<<0.5 * alas * tinggi;
-              break;
-        case '3' :
-                   cout<<"Menghitung luas lingkaran"<<endl;
-                   cout<<"Jari-jari = "; 
-				   cin>>jari;
-                   cout<<setprecision(8);
-                   cout<<"Luas lingkaran = "<<phi * jari * jari;
-              break;
-         default :
-                   cout<<"Pilihan anda tidak ada di sistem"<<endl;
+    switch(pilih){
+    case '1' :
+    	
+    cout<<"Menghitung luas persegi"<<endl;
+    cout<<"Sisi = ";
+	cin>>sisi;
+    cout<<"Luas persegi = "<<sisi*sisi;
+    
+    break;
+    case '2' :
+    	
+    cout<<"Menghitung luas segitiga"<<endl;
+    cout<<"Alas = ";
+	cin>>alas;
+    cout<<"Tinggi = ";
+	cin>>tinggi;
+    cout<<"Luas segitiga = "<<0.5 * alas * tinggi;
+    
+    break;
+    case '3' :
+    	
+    cout<<"Menghitung luas lingkaran"<<endl;
+    cout<<"Jari-jari = "; 
+	cin>>jari;
+    cout<<setprecision(8);
+    cout<<"Luas lingkaran = "<<phi * jari * jari;
+    
+    break;
+    default :
+    	
+    cout<<"Pilihan anda tidak ada di sistem"<<endl;
+    
     }
-            cout<<"\nKembali ke menu utama ? (y/n) "; 
-			cin>>ulang; //untuk      kembali ke dalam blok do-while
+    
+    cout<<"\nKembali ke menu utama ? (y/n) "; 
+	cin>>ulang; //untuk      kembali ke dalam blok do-while
 
-      }while(ulang == 'y' || ulang == 'Y'); 
+    }
+    
+	while(ulang == 'y' || ulang == 'Y'); 
 }
